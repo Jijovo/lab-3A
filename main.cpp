43f8e4085e7f28d0c428e1bbe522fc9d3b4e1698
Jijovo <jijovo77@gmail.com>
2026-02-08 10:25:00 -0800

    second

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 using namespace std;
 #include <iostream>
 
+
+
+struct restaurant {
+    string name;
+    string city;
+    string country;
+    int zipCode;
+    double rating;
+
+}
+
+restaurant createRestaurant();
+
+restaurant createRestaurant() {
+    restaurant r;
+    cout << "Enter restaurant name: ";
+    cin >> r.name;
+    cout << "Enter restaurant city: ";
+    cin >> r.city;
+    cout << "Enter restaurant country: ";
+    cin >> r.country;
+    cout << "Enter restaurant zip code: ";
+    cin >> r.zipCode;
+    cout << "Enter restaurant rating: ";
+    cin >> r.rating;
+    return r;
+}
+
 int main() {
+    restaurant r = createRestaurant();
+    cout << r.name << endl << r.city << endl << r.country << endl << r.zipCode << endl << r.rating << endl;
     return 0;
 }
